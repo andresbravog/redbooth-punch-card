abe398f434bd7882e6aa53ffff0c002e590e306e
andresbravog <andresbravog@gmail.com>
2014-11-17 11:42:30 +0100

    Improve redbooth

--- app/services/redbooth_connector/base.rb
+++ app/services/redbooth_connector/base.rb
@@ -3,16 +3,26 @@ require 'redbooth-ruby'
 class RedboothConnector::Base
   attr_accessor :user
 
+  # Initializes the Ruby object with the user holding
+  # the redbooth credentials
+  #
+  # @param user [User] user holding the redbooth credentials
   def initialize(user)
     @user = user
   end
 
   protected
 
+  # Creates RedboothRuby session based on user credentials
+  #
+  # @return [RedboothRuby::Session]
   def session
     @session ||= ::RedboothRuby::Session.new(token: user.credentials.token)
   end
 
+  # Creates RedboothRuby client based on the session
+  #
+  # @return [RedboothRuby::Client]
   def client
     @client ||= ::RedboothRuby::Client.new(session)
   end
